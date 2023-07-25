b1e07150b852a0bad3e8412cb0a6824e418305bd
Forrest Verellen <fsverellen@mail.com>
2023-07-25 15:38:41 -0400

    second try

--- Used Car Lot/Cars.cs
+++ Used Car Lot/Cars.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+
 namespace Used_Car_Lot
 {
 	public class Cars
 	{
-		public Cars()
+		//properties
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+		public decimal Price { get; set; }
+
+		//constructor
+		public Cars ()
 		{
+			Make = "Ford";
+			Model = "Taurus";
+			Year = 2002;
+			Price = 2500.00m;
+		}
+		public Cars(string _make, string _model, int _year, decimal _price)
+		{
+			Make = _make;
+			Model = _model;
+			Year = _year;
+			Price = _price;
+
 		}
 	}
 }
